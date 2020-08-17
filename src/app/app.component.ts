2e4562830d813eb409edc82eb1d80a168837152e
ZeevKatz <zeevk6@gmail.com>
2020-08-17 09:44:09 +0300

    feat: 🎸 use ReactiveHook in demo app for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ReactiveHook, Hook } from '@ngze/reactive-hooks';
+import { Observable } from 'rxjs';
+import { mapTo, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ` <strong>Number of checks: {{ numberOfChecks$ | async }}</strong> `,
+  styles: [
+    `
+      :host {
+        display: flex;
+        height: 100vh;
+        align-items: center;
+        justify-content: center;
+      }
+    `,
+  ],
 })
 export class AppComponent {
-  title = 'reactive-hooks';
+  @ReactiveHook(Hook.DoCheck)
+  private readonly doCheck$: Observable<void>;
+
+  readonly numberOfChecks$ = this.doCheck$.pipe(
+    mapTo(1),
+    scan((acc, value) => acc + value)
+  );
+
+  @HostListener('mousemove')
+  // tslint:disable-next-line:typedef
+  onMousemove() {}
 }
