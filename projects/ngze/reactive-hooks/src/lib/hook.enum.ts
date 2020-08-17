0cd644cdde55f502bcc9f10865829345aaeddebe
ZeevKatz <zeevk6@gmail.com>
2020-08-17 09:27:44 +0300

    feat: 🎸 add enum for lifecycle hooks

---
+++ projects/ngze/reactive-hooks/src/lib/hook.enum.ts
@@ -0,0 +1,13 @@
+/**
+ * Enumeration of the available lifecycle hooks.
+ */
+export const enum Hook {
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterViewInit,
+  AfterViewChecked,
+  AfterContentInit,
+  AfterContentChecked,
+  OnDestroy,
+}
