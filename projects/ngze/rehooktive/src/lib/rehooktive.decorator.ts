0548a767f757035efb3403dff33c24bd613be9c6
ZeevKatz <zeevk6@gmail.com>
2020-08-23 21:29:51 +0300

    refactor: 💡 move internal logic into internals file

--- projects/ngze/rehooktive/src/lib/rehooktive.decorator.ts
+++ projects/ngze/rehooktive/src/lib/rehooktive.decorator.ts
@@ -1,19 +1,16 @@
 import { Observable, Subject } from 'rxjs';
-import { Type, ɵivyEnabled as ivyEnabled } from '@angular/core';
+import { Type } from '@angular/core';
 
 import { HooksConfigs } from './hooks-configs';
 import { Hook } from './hook.enum';
 import { HookConfig } from './hook-config.interface';
+import { addHookMethodOnTargetIfMissing, completeSubjectOnInstance } from './internals';
 
-export const isFunction = (value: unknown): value is () => {} => {
-  return typeof value === 'function';
-};
-
-const setupHookSubjectOnTarget = (target: Type<unknown>, config: HookConfig) => {
-  const subjectStoreProperty = '_'.concat(config.subjectPropertyName.toString());
+const setupHookSubjectOnTarget = (target: Type<unknown>, subjectPropertyName: symbol) => {
+  const subjectStoreProperty = '_'.concat(subjectPropertyName.toString());
 
   Object.defineProperties(target, {
-    [config.subjectPropertyName]: {
+    [subjectPropertyName]: {
       get(): Subject<unknown> {
         return (this[subjectStoreProperty] = this[subjectStoreProperty] || new Subject());
       },
@@ -21,32 +18,30 @@ const setupHookSubjectOnTarget = (target: Type<unknown>, config: HookConfig) => 
   });
 };
 
-const completeSubjectOnInstance = (instance: object, config: HookConfig) => {
-  instance[config.subjectPropertyName].next();
-  instance[config.subjectPropertyName].complete();
-  instance[config.subjectPropertyName] = null;
-};
-
-const setupHookMethodOnTarget = (target: Type<unknown>, config: HookConfig) => {
-  const originalMethod = target[config.methodName];
+const setupHookMethodOnTarget = (target: Type<unknown>, methodName: string, subjectPropertyName: symbol) => {
+  const originalMethod = target[methodName];
 
   Object.defineProperties(target, {
-    [config.methodName]: {
+    [methodName]: {
       value(...args: unknown[]): void {
-        this[config.subjectPropertyName].next(args[0]);
+        this[subjectPropertyName].next(args[0]);
         originalMethod.apply(this, ...args);
       },
     },
   });
 };
 
-const setupHookObservableOnTargetProperty = (target: Type<unknown>, key: string | symbol, config: HookConfig) => {
+const setupHookObservableOnTargetProperty = (
+  target: Type<unknown>,
+  key: string | symbol,
+  subjectPropertyName: symbol
+) => {
   Object.defineProperties(target, {
     [key]: {
       enumerable: true,
       configurable: true,
       get(): Observable<unknown> {
-        return this[config.subjectPropertyName].pipe();
+        return this[subjectPropertyName].pipe();
       },
     },
   });
@@ -58,24 +53,12 @@ const setupOnDestroyOnTarget = (target: Type<unknown>, config: HookConfig, destr
   Object.defineProperties(target, {
     [destroyConfig.methodName]: {
       value(): void {
-        completeSubjectOnInstance(this, config);
-        completeSubjectOnInstance(this, destroyConfig);
+        completeSubjectOnInstance(this, config.subjectPropertyName);
+        completeSubjectOnInstance(this, destroyConfig.subjectPropertyName);
         originalOnDestroyMethod.apply(this);
       },
     },
   });
-};
-
-const addHookMethodOnTargetIfMissing = (target: Type<unknown>, config: HookConfig) => {
-  if (!isFunction(target[config.methodName])) {
-    if (ivyEnabled) {
-      target.constructor.prototype[config.methodName] = () => {};
-    } else {
-      throw new Error(
-        `${target.constructor.name} is using Rehooktive for ${config.methodName} but it wasn't implemented.`
-      );
-    }
-  }
 };
 
 /**
@@ -86,12 +69,12 @@ export function Rehooktive(hook: Hook): PropertyDecorator {
     const config = HooksConfigs[hook];
     const destroyConfig = HooksConfigs[Hook.OnDestroy];
 
-    addHookMethodOnTargetIfMissing(target, config);
-    addHookMethodOnTargetIfMissing(target, destroyConfig);
+    addHookMethodOnTargetIfMissing(target, config.methodName);
+    addHookMethodOnTargetIfMissing(target, destroyConfig.methodName);
 
-    setupHookSubjectOnTarget(target, config);
-    setupHookObservableOnTargetProperty(target, key, config);
-    setupHookMethodOnTarget(target, config);
+    setupHookSubjectOnTarget(target, config.subjectPropertyName);
+    setupHookObservableOnTargetProperty(target, key, config.subjectPropertyName);
+    setupHookMethodOnTarget(target, config.methodName, config.subjectPropertyName);
     setupOnDestroyOnTarget(target, config, destroyConfig);
   };
 }
