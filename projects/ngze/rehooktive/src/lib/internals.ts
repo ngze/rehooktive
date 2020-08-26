cea7b8204a47f22ded9b9a1cc8b81097b47fef27
ZeevKatz <zeevk6@gmail.com>
2020-08-26 21:27:05 +0300

    docs: ✏️ add missing docs

--- projects/ngze/rehooktive/src/lib/internals.ts
+++ projects/ngze/rehooktive/src/lib/internals.ts
@@ -1,13 +1,23 @@
 import { Type, ɵivyEnabled as ivyEnabled } from '@angular/core';
 
+/**
+ * Type guard for functions.
+ */
 export const isFunction = (value: unknown): value is () => {} => typeof value === 'function';
 
+/**
+ * Completes subject on the provided instance by property name.
+ */
 export const completeSubjectOnInstance = (instance: object, subjectPropertyName: symbol) => {
   instance[subjectPropertyName].next();
   instance[subjectPropertyName].complete();
   instance[subjectPropertyName] = null;
 };
 
+/**
+ * Adds lifecycle hook method if missing.
+ * Throws an error when Ivy is disabled/not available.
+ */
 export const addHookMethodOnTargetIfMissing = (target: Type<unknown>, methodName: string) => {
   if (!isFunction(target[methodName])) {
     if (ivyEnabled) {
